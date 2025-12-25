40c1b179a55a2cda402b67ab16cf568b29ed0cf1
Muhammad Asif Shabbir <74806002+yasirraheel@users.noreply.github.com>
2025-12-25 14:19:11 +0500

    feat(ui): add verified badge to user sidebar and ticket views

--- resources/views/frontend/default/include/common/user-content.blade.php
+++ resources/views/frontend/default/include/common/user-content.blade.php
@@ -9,7 +9,15 @@
                 </a>
             @endif
         </div>
-        <h6 class="text-muted">{{ '@' . $user->username }}</h6>
+        <h6 class="text-muted">
+            {{ '@' . $user->username }}
+            @if($user->kyc == 1)
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#3b82f6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: text-bottom; margin-left: 2px;" title="{{ __('Verified Seller') }}" data-bs-toggle="tooltip">
+                    <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.78 4.78 4 4 0 0 1-6.74 0 4 4 0 0 1-4.78-4.78 4 4 0 0 1 0-6.74z"></path>
+                    <polyline points="9 11 12 14 22 4"></polyline>
+                </svg>
+            @endif
+        </h6>
     </div>
     @if (setting($user->user_type . '_deposit', 'permission') && $user->deposit_status)
         <div class="topup">
--- resources/views/frontend/default/ticket/show.blade.php
+++ resources/views/frontend/default/ticket/show.blade.php
@@ -72,7 +72,15 @@
                         <div class="user-info">
                             @if ($message->model == 'user')
                                 <h4>{{ $user->full_name }}</h4>
-                                <p>{{ '@' . $user->username }}</p>
+                                <p>
+                                    {{ '@' . $user->username }}
+                                    @if($user->kyc == 1)
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="#3b82f6" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: text-bottom; margin-left: 2px;" title="{{ __('Verified Seller') }}" data-bs-toggle="tooltip">
+                                            <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.78 4.78 4 4 0 0 1-6.74 0 4 4 0 0 1-4.78-4.78 4 4 0 0 1 0-6.74z"></path>
+                                            <polyline points="9 11 12 14 22 4"></polyline>
+                                        </svg>
+                                    @endif
+                                </p>
                             @else
                                 <h4>{{ $message->user->name }}</h4>
                             @endif
